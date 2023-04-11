1b52b9dff2e71d3fedb4733693be7a3620faee21
Gayatri Vadaparty <67108072+GayatriVadaparty@users.noreply.github.com>
2023-04-11 07:13:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const context = canvas.getContext('2d');
 const width = 500;
 const height = 700;
 const screenWidth = window.screen.width;
-const canvasPosition = screenWidth / 3 - width / 3;
+const canvasPosition = screenWidth / 2 - width / 2;
 const isMobile = window.matchMedia('(max-width: 600px)');
 const gameOverEl = document.createElement('div');
 
